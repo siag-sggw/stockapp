b059e984f55adca81949cd4fecc549dd70e7886e
Unknown <wiktor.elattar@gmail.com>
2019-01-22 19:13:54 +0100

    login page design

--- lib/stock_login.dart
+++ lib/stock_login.dart
@@ -16,18 +16,32 @@ class LoginState extends State<Login> {
     @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Sign In")),
       body: new Container(
           padding: const EdgeInsets.all(20.0),
           child: new Center(
             child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                    new RaisedButton(
-                        onPressed: this._signin,
-                        child: new Text("Sign In"),
-                        color: Colors.lightBlueAccent,
-                      ),
+                new Hero(
+                  tag: 'hero',
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: 128.0,
+                    child: Image.asset('assets/increase.png'),
+                  ),
+                ),
+                new ButtonTheme(
+                  minWidth: 250.0,
+                  height: 50.0,
+                  child: new RaisedButton(
+                    onPressed: this._signin,
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                    child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                    color: Colors.lightBlueAccent,
+                  )
+                )
                 //new RaisedButton(onPressed: this.onLogout, child: new Text("Logout"), color: Colors.amberAccent),
               ],
             ),
